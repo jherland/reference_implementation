95abd428e40746837be0733626e6c3db53ca05f7
Johan Herland <johan@herland.net>
2020-04-11 22:28:41 +0200

    example_run.py: Refactor common patterns into a World helper class
    The example code repeatedly advances to the next day. Collect
    timekeeping and the need to call each MockApp's .next_day() method
    into a helper class. This hopefully aids readability of the example
    code.

--- example_run.py
+++ example_run.py
@@ -22,54 +22,65 @@ from datetime import date, datetime, time, timedelta
 import LowCostDP3T
 
 
-def epochs(
-	start=datetime.combine(date.today(), time.min),
-	end=datetime.combine(date.today(), time.max),
-):
-	''' Return datetime objects for each epoch between `start` and `end`.
+class World:
+	def __init__(self, start_date):
+		self.today = start_date
+		self.people = []
 
-	For example, assuming each epoch is 15 minutes long, calling epochs() with
-	start = datetime.fromisoformat("2020-04-01 08:00") and
-	end = datetime.fromisoformat("2020-04-01 10:00") will yield these objects:
-		datetime(2020, 4, 1, 8, 0, 0)
-		datetime(2020, 4, 1, 8, 15, 0)
-		datetime(2020, 4, 1, 8, 30, 0)
-		datetime(2020, 4, 1, 8, 45, 0)
-		datetime(2020, 4, 1, 9, 0, 0)
-		datetime(2020, 4, 1, 9, 15, 0)
-		datetime(2020, 4, 1, 9, 30, 0)
-		datetime(2020, 4, 1, 9, 45, 0)
+	def add_people(self, *people):
+		self.people.extend(people)
 
-	If `start` and/or `end` are not given they default to the start (0:00)
-	and end (23:59) of today, respectively.
-	'''
-	epoch = timedelta(minutes=LowCostDP3T.EPOCH_LENGTH)
-	while start < end:
-		yield start
-		start += epoch
+	def next_day(self):
+		''' Add 24h to world clock and advance all people to the next day.
+		'''
+		self.today += timedelta(days=1)
+		for p in self.people:
+			p.next_day()
+
+	def epochs(self, start=time.min, end=time.max):
+		''' Return datetime objects for each epoch between `start` and `end`.
+
+		For example, given that today's date is 2020-04-01 and each epoch is
+		15 minutes, calling self.epochs(time(8, 0, 0), time(10, 0, 0)) will
+		yield the following datetimes:
+			datetime(2020, 4, 1, 8, 0, 0)
+			datetime(2020, 4, 1, 8, 15, 0)
+			datetime(2020, 4, 1, 8, 30, 0)
+			datetime(2020, 4, 1, 8, 45, 0)
+			datetime(2020, 4, 1, 9, 0, 0)
+			datetime(2020, 4, 1, 9, 15, 0)
+			datetime(2020, 4, 1, 9, 30, 0)
+			datetime(2020, 4, 1, 9, 45, 0)
+
+		If `start` and/or `end` are not given they default to the start (0:00)
+		and end (23:59) of the day, respectively.
+		'''
+		epoch = timedelta(minutes=LowCostDP3T.EPOCH_LENGTH)
+		now = datetime.combine(self.today, start)
+		end = datetime.combine(self.today, end)
+		while now < end:
+			yield now
+			now += epoch
 
 
 def main():
 	# Mock time starts midnight on April 01.
-	epotime = datetime(2020, 4, 1)
+	world = World(date(2020, 4, 1))
 	
 	# We have three people: Alice, Bob, and Isidor
 	alice = LowCostDP3T.MockApp()
 	bob = LowCostDP3T.MockApp()
 	isidor = LowCostDP3T.MockApp()
+	world.add_people(alice, bob, isidor)
 
 	# Run tests for the specified number of days
 	for day in range(2):
 		print("Day: Alice, Bob, and Isidor do not have contact.")
-		epotime += timedelta(days=1)
-		alice.next_day()
-		bob.next_day()
-		isidor.next_day()
+		world.next_day()
 
 	for day in range(3):
 		print("Day: Alice and Bob work in the same office, Isidor elsewhere.")
-		# For each epoch between 08:00 and 17:00
-		for now in epochs(epotime.replace(hour=8), epotime.replace(hour=17)):
+		for now in world.epochs(start=time(8, 0, 0), end=time(17, 0, 0)):
 			alice_ephID = alice.keystore.get_current_ephID(now)
 			bob_ephID = bob.keystore.get_current_ephID(now)
 			# Record two beacons in the same epoch, resulting in a contact
@@ -82,13 +93,11 @@ def main():
 			alice.next_epoch()
 			bob.next_epoch()
 		# Tik Tok
-		epotime += timedelta(days=1)
-		alice.next_day()
-		bob.next_day()
-		isidor.next_day()        
+		world.next_day()
+
 
 	print("Day: Bob and Isidor meet for dinner.")
-	for now in epochs(epotime.replace(hour=17), epotime.replace(hour=20)):
+	for now in world.epochs(start=time(17, 0, 0), end=time(20, 0, 0)):
 		bob_ephID = bob.keystore.get_current_ephID(now)
 		isidor_ephID = isidor.keystore.get_current_ephID(now)
 		# Record two beacons in the same epoch, resulting in a contact
@@ -103,21 +112,26 @@ def main():
 		isidor.next_epoch()
 
 	print("Isidor is tested positive.")
-	infectious_date = epotime
+	infectious_date = world.today
 	infections_SK = isidor.keystore.SKt[0]
 
 	# Tik Tok
-	epotime += timedelta(days=1)
-	alice.next_day()
-	bob.next_day()
-	isidor.next_day()
+	world.next_day()
 
 	# Check infectiousness
 	print("Check exposure of Alice and Bob.")
 	print("Alice: (not positive)")
-	alice.ctmgr.check_infected(infections_SK, infectious_date.strftime("%Y-%m-%d"), epotime)
+	alice.ctmgr.check_infected(
+		infections_SK,
+		infectious_date.strftime("%Y-%m-%d"),
+		datetime.combine(world.today, time(0, 0, 0)),
+	)
 	print("Bob: (at risk)")
-	bob.ctmgr.check_infected(infections_SK, infectious_date.strftime("%Y-%m-%d"), epotime)
+	bob.ctmgr.check_infected(
+		infections_SK,
+		infectious_date.strftime("%Y-%m-%d"),
+		datetime.combine(world.today, time(0, 0, 0)),
+	)
 
 
 if __name__ == "__main__":
